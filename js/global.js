06c0d6c8c490d638d138341bcd961620a850fdd4
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-06-21 16:54:12 -0400

    Can now pull latitude and longitude from clutertruck kitchen API

--- js/global.js
+++ js/global.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
+    var selectedCity = null;
+    var selectedLatitude = null;
+    var selectedLongitude =  null;
 
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.staging.clustertruck.com/api/kitchens/',
+        success: function(cluster) {
+            // $.each(cluster, function(i, cluster){
+            //     console.log(cluster.location.lat, cluster.location.lng);
+            // });
+
+            console.log(cluster);
+
+            $('.citySelection').on('click', function() {
+                var cityId = $(this).attr('id');
+                var cityElement = $(this).attr('data-id-number');
+                selectedCity = $('#'+cityId).html();
+                selectedLatitude = cluster[cityElement].location.lat;
+                selectedLongitude = cluster[cityElement].location.lng;
+
+                console.log(selectedCity, selectedLatitude, selectedLongitude);
+            }) 
+
+            $('.daySelection').on('click', function() {
+                var dayId = $(this).attr('id');
+                var content = $('#'+dayId).html();
+                $('.selectedDayDate').html(content);
+            })
+
+        }
+    });   
 });
